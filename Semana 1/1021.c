ff1775df27bd651431522fa86be35f1a72798412
matheusfideles <67912259+matheusfideles@users.noreply.github.com>
2020-07-09 16:44:59 -0300

    Update 1021.c
    Comentei o código para ficar mais fácil de se visualizar.

--- Semana 1/1021.c
+++ Semana 1/1021.c
@@ -6,33 +6,57 @@ int main() {
     double n;
 
     scanf("%lf", &n);
-    int notas = n; 
-    int moedas = (n - notas) * 100;
+    int notas = n; //Separa o valor lido em duas partes, uma contendo a parte em reais/notas
+    int moedas = (n - notas) * 100;//e a outra parte em centavos/moedas
 
+    //Conta as notas de 100 necessárias e retira o valor do total de notas
     notasDe100 = notas/100;
     notas = notas%100;
+
+    //Conta as notas de 50 necessárias e retira o valor do total de notas
     notasDe50 = notas/50;
     notas = notas%50;
+
+    //Conta as notas de 20 necessárias e retira o valor do total de notas
     notasDe20 = notas/20;
     notas = notas%20;
+
+    //Conta as notas de 10 necessárias e retira o valor do total de notas
     notasDe10 = notas/10;
     notas = notas%10;
+
+    //Conta as notas de 5 necessárias e retira o valor do total de notas
     notasDe5 = notas/5;
     notas = notas%5;
+
+    //Conta as notas de 2 necessárias e retira o valor do total de notas
     notasDe2 = notas/2;
     notas = notas%2;
 
+    //O valor que sobrar das operaçoes anteriores será a quantidade de moedas de 1 real
     moedasDe1r = notas;
+
+    //Conta as moedas de 50c e retira o valor do total de moedas
     moedasDe50c = moedas/50;
     moedas = moedas%50;
+
+    //Conta as moedas de 25c e retira o valor do total de moedas
     moedasDe25c = moedas/25;
     moedas = moedas%25;
+
+    //Conta as moedas de 10c e retira o valor do total de moedas
     moedasDe10c = moedas/10;
     moedas = moedas%10;
+
+    //Conta as moedas de 5c e retira o valor do total de moedas
     moedasDe5c = moedas/5;
     moedas = moedas%5;
+
+    //O valor que restar das operações com as moedas será dado em moedas de um centavo
     moedasDe1c = moedas;
 
+
+    //Exibe o resultado
     printf("NOTAS:\n");
     printf("%d nota(s) de R$ 100.00\n", notasDe100);
     printf("%d nota(s) de R$ 50.00\n", notasDe50);
