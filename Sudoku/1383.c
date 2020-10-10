51762a0cefecd4e5ebc1b7325764d217edc51ec9
matheusfideles <67912259+matheusfideles@users.noreply.github.com>
2020-10-09 23:21:24 -0300

    Update 1383.c
    Código refeito descartando a lógica da soma de quadrados, que é incorreta.

--- Sudoku/1383.c
+++ Sudoku/1383.c
@@ -1,30 +1,16 @@
 #include <stdio.h>
-#include <math.h>
 int main()
 {
-    int quantMat=0, lin=0, col=0, mat[9][9], a=0, b=0, aux=0, resp=1, somaGrade[3][3], cont[9], soma=285;
-    //285 = 1^2 + 2^2 + 3^2 + 4^2 + 5^2 + 6^2 + 7^2 + 8^2 + 9^2
-    //Não há como outros numeros, dentro do sudoku, darem o mesmo resultado.   
+    int checaLinha(int mat[9][9]);
+    int checaColuna(int mat[9][9]);
+    int checa3x3(int mat[9][9], int linhaInicio, int colunaInicio, int linhaFim, int colunaFim);
+    int checaTodas3x3(int mat[9][9]);
+    int quantMat=0, lin=0, col=0, mat[9][9], a=0, resp=1; 
 
     scanf("%d", &quantMat);
 
     for(a=0; a<quantMat; a++)
     {
-        //Inicializando os indices em somaGrade com 0
-        for(lin=0; lin<3; lin++)
-        {
-            for(col=0; col<3; col++)
-            {
-                somaGrade[lin][col] = 0;
-            }
-        }
-
-        //Inicializando os indices em cont
-        for(b=0; b<9; b++)
-        {
-            cont[b] = 0;
-        }
-
         //Leitura dos valores
         for(lin=0; lin<9; lin++)
         {
@@ -34,122 +20,13 @@ int main()
             }
         }
     
-        //Compara linha
-        for(lin=0; lin<9; lin++)
+        //Checa os numeros na linha e na coluna
+        resp = checaLinha(mat) && checaColuna(mat);
+    
+        //Checa as matrizes 3x3 da matriz 9x9, caso a matriz ainda seja considerada válida pelo teste de linhas e colunas
+        if(resp==1)
         {
-            for(col=0; col<9; col++)
-            {
-                switch (mat[lin][col])//Conta a aparição dos numeros de 1 a 9 na linha
-                {
-                    case 1: 
-                        cont[0]++;
-                        break;
-                    case 2:
-                        cont[1]++;
-                        break;
-                    case 3:
-                        cont[2]++;
-                        break;
-                    case 4:
-                        cont[3]++;
-                        break;
-                    case 5:
-                        cont[4]++;
-                        break;
-                    case 6:
-                        cont[5]++;
-                        break;
-                    case 7:
-                        cont[6]++;
-                        break;
-                    case 8:
-                        cont[7]++;
-                        break;
-                    case 9:
-                        cont[8]++;
-                        break;
-                    default:
-                        resp=0;
-                }
-            }
-            for(b=0; b<9;b++)//Checa se temos exatamente 1 de cada número de 1-9
-            {
-                if(cont[b] != 1)
-                {
-                    resp=0;
-                }
-                cont[b]=0;
-            }
-        }
-
-        //Compara coluna
-        for(col=0; col<9; col++)
-        {
-            for(lin=0; lin<9; lin++)
-            {
-                switch (mat[lin][col])//Conta a aparição dos numeros de 1 a 9 na coluna
-                {
-                    case 1: 
-                        cont[0]++;
-                        break;
-                    case 2:
-                        cont[1]++;
-                        break;
-                    case 3:
-                        cont[2]++;
-                        break;
-                    case 4:
-                        cont[3]++;
-                        break;
-                    case 5:
-                        cont[4]++;
-                        break;
-                    case 6:
-                        cont[5]++;
-                        break;
-                    case 7:
-                        cont[6]++;
-                        break;
-                    case 8:
-                        cont[7]++;
-                        break;
-                    case 9:
-                        cont[8]++;
-                        break;
-                    default:
-                        resp=0;
-                }
-            }
-            for(b=0; b<9;b++)//Checa pra ver se temos exatamente 1 de cada número na coluna.
-            {
-                if(cont[b] != 1)
-                {
-                    resp=0;
-                }
-                cont[b]=0;
-            }
-        }
-
-        //Soma dos quadrados internos da grade
-        for(lin=0; lin<9; lin++)
-        {
-            for(col=0; col<9; col++)
-            {
-                aux = pow(mat[lin][col],2);//Elevamos ao quadrado para podermos comparar a soma a 285
-                somaGrade[lin/3][col/3] += aux;
-            }
-        }
-        
-        //Não deve ser diferente de 285. 285 é a soma de 1^2 ~ 9^2
-        for(lin=0; lin<3; lin++)
-        {
-            for(col=0; col<3; col++)
-            {
-                if(somaGrade[lin][col] != soma)//A soma do quadrado dos valores dentro de uma grade 3x3 não pode ser diferente de 285
-                {
-                    resp=0;
-                }
-            }
+            resp = checaTodas3x3(mat);
         }
 
         //Imprime resposta
@@ -165,4 +42,203 @@ int main()
         resp=1;
     }
     return 0;   
+}
+
+int checaLinha(int mat[9][9])
+{
+    int lin=0, col=0, resp=1, cont[9], i=0;
+
+    //Inicializando os indices em cont, que irá contar a aparição dos numeros de 1-9 em cada um dos indices da linha/coluna
+    //o indice [0] irá contabilizar a aparição do numero 1, o [1] a do numero 2 e assim por diante...
+    for(i=0; i<9; i++)
+    {
+        cont[i]=0;
+    }
+
+    //Checa a aparição dos numeros na linha
+    for(lin=0; lin<9; lin++)
+    {
+        for(col=0; col<9; col++)
+        {
+            switch (mat[lin][col])//Conta a aparição dos numeros de 1 a 9 na linha
+            {
+                case 1: 
+                    cont[0]++;
+                    break;
+                case 2:
+                    cont[1]++;
+                    break;
+                case 3:
+                    cont[2]++;
+                    break;
+                case 4:
+                    cont[3]++;
+                    break;
+                case 5:
+                    cont[4]++;
+                    break;
+                case 6:
+                    cont[5]++;
+                    break;
+                case 7:
+                    cont[6]++;
+                    break;
+                case 8:
+                    cont[7]++;
+                    break;
+                case 9:
+                    cont[8]++;
+                    break;
+                default://Caso apareça um numero que não esteja entre 1 e 9, a matriz estará incorreta
+                    resp=0;
+            }
+        }
+        for(i=0; i<9;i++)//Checa se um número se repetiu ou não esteve presente na linha
+        {
+            if(cont[i] != 1)//Se não tivermos exatamente 1 de cada número na linha, a matriz não sera uma solução
+            {
+                resp=0;
+            }
+            cont[i]=0;//Reinicia os indices em cont para realizar a proxima iteração do laço
+        }
+    }
+    return resp;
+}
+
+int checaColuna(int mat[9][9])
+{
+    int lin=0, col=0, resp=1, cont[9], i=0;
+    //Inicializando os indices em cont, que irá contar a aparição dos numeros de 1-9 em cada um dos indices da coluna
+    //o indice [0] irá contabilizar a aparição do numero 1, o [1] a do numero 2 e assim por diante...
+    for(i=0; i<9; i++)
+    {
+        cont[i] = 0;
+    }
+
+    //Checa a aparição dos numeros na coluna
+    for(col=0; col<9; col++)
+    {
+        for(lin=0; lin<9; lin++)
+        {
+            switch (mat[lin][col])//Conta a aparição dos numeros de 1 a 9 na coluna
+            {
+                case 1: 
+                    cont[0]++;
+                    break;
+                case 2:
+                    cont[1]++;
+                    break;
+                case 3:
+                    cont[2]++;
+                    break;
+                case 4:
+                    cont[3]++;
+                    break;
+                case 5:
+                    cont[4]++;
+                    break;
+                case 6:
+                    cont[5]++;
+                    break;
+                case 7:
+                    cont[6]++;
+                    break;
+                case 8:
+                    cont[7]++;
+                    break;
+                case 9:
+                    cont[8]++;
+                    break;
+                default://Caso não apareça um numero que esteja entre 1 e 9, a matriz estará incorreta
+                    resp=0;
+            }
+        }
+        for(i=0; i<9;i++)//Checa se um número se repetiu ou não esteve presente na linha
+        {
+            if(cont[i] != 1)//Se não tivermos exatamente 1 de cada número na linha, a matriz não sera uma solução
+            {
+                resp=0;
+            }
+            cont[i]=0;//Reinicia os indices em cont para realizar a proxima iteração do laço
+        }
+    }
+    return resp;
+}
+
+int checa3x3(int mat[9][9], int linhaInicio, int colunaInicio, int linhaFim, int colunaFim)
+{
+    int i=0, j=0, cont[9], resp=1;
+
+    //Inicia os indices no vetor cont, que irá contar a aparição dos numeros de 1 a 9 na submatriz 3x3
+    //cont irá armazenar no indice [0] o número de aparições do numero 1, [1] irá armazenar a aparição do numero 2 e assim por diante...
+    for(i=0; i<9; i++)
+    {
+        cont[i]=0;
+    }
+
+    //Percorre a matriz 9x9 no intervalo dado nos parametros
+    for(i=linhaInicio; i<=linhaFim; i++)
+    {
+        for(j=colunaInicio; j<=colunaFim; j++)
+        {
+            switch (mat[i][j])//Conta a aparição dos numeros de 1 a 9 na matriz 3x3
+            {
+                case 1: 
+                    cont[0]++;
+                    break;
+                case 2:
+                    cont[1]++;
+                    break;
+                case 3:
+                    cont[2]++;
+                    break;
+                case 4:
+                    cont[3]++;
+                    break;
+                case 5:
+                    cont[4]++;
+                    break;
+                case 6:
+                    cont[5]++;
+                    break;
+                case 7:
+                    cont[6]++;
+                    break;
+                case 8:
+                    cont[7]++;
+                    break;
+                case 9:
+                    cont[8]++;
+                    break;
+                default://Caso não apareça um numero que esteja entre 1 e 9, a matriz 9x9 estará incorreta
+                    resp=0;
+            }
+        }
+    }
+
+    //Checa se tivemos numeros repetidos ou que estão faltando na matriz 3x3
+    for(i=0; i<9; i++)
+    {
+        if(cont[i]!=1)//Se não tivermos exatamente 1 de cada número na matriz 3x3, a matriz 9x9 não será uma solução
+        {
+            resp=0;
+        }
+    }
+    return resp;
+}
+
+int checaTodas3x3(int mat[9][9])
+{
+    int resp=1;
+    //Confere se todas as submatrizes 3x3 estão corretas
+    resp = resp && checa3x3(mat, 0, 0, 2, 2);
+    resp = resp && checa3x3(mat, 0, 3, 2, 5);
+    resp = resp && checa3x3(mat, 0, 6, 2, 8);
+    resp = resp && checa3x3(mat, 3, 0, 5, 2);
+    resp = resp && checa3x3(mat, 3, 3, 5, 5);
+    resp = resp && checa3x3(mat, 3, 6, 5, 8);
+    resp = resp && checa3x3(mat, 6, 0, 8, 2);
+    resp = resp && checa3x3(mat, 6, 3, 8, 5);
+    resp = resp && checa3x3(mat, 6, 6, 8, 8);
+    return resp;
 }
